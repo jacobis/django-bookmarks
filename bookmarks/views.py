b827e53837875d89734109007c30e2df2ff6583e
Jacob Lee <remind.jacob@gmail.com>
2013-12-28 18:11:03 +0900

    Main page template, view modified, CSRF token added

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -3,17 +3,14 @@
 from django.http import HttpResponse, Http404
 from django.template import Context
 from django.template.loader import get_template
+from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
 
 def main_page(request):
-    template = get_template('main_page.html')
-    variables = Context({
-        'head_title': '장고 북마크',
-        'page_title': '장고 북마크에 오신 것을 환영합니다.',
-        'page_body': '여기에 북마크를 저장하고 공유할 수 있습니다!'
-        })
-    output = template.render(variables)
-    return HttpResponse(output)
+    return render_to_response(
+        'main_page.html',
+        {'user': request.user}
+    )
 
 def user_page(request, username):
     try:
