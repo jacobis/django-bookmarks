f06a39ff24506e0d5f9aaed4d24eae569ff5b103
Jacob Lee <remind.jacob@gmail.com>
2014-01-02 01:19:46 +0900

    Django extensions app added

--- settings.py
+++ settings.py
@@ -123,6 +123,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+
     'django-bookmarks.bookmarks',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
