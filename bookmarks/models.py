a5a52bd334cc0c7d48dee0184c566d7398aba103
Jacob Lee <remind.jacob@gmail.com>
2014-01-02 01:13:24 +0900

    Bookmarks tag added

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -11,3 +11,8 @@ class Bookmark(models.Model):
     title = models.CharField(max_length=200)
     user = models.ForeignKey(User)
     link = models.ForeignKey(Link)
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=64, unique=True)
+	bookmarks = models.ManyToManyField(Bookmark)
